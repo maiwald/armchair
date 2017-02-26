d9666542a09ee9716fe81089acf22a55498d1fee
Luciano Maiwald <luciano.maiwald@gmail.com>
2017-02-26 20:34:29 +0100

    remove unnecessary logic folder

--- logic/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to armchair
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- logic/test/armchair/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns armchair.core-test
-  (:require [clojure.test :refer :all]
-            [armchair.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
