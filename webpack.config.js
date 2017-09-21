ef7dd0af2a8b0d3be774ca5254ffa225b4464c27
Luciano Maiwald <luciano.maiwald@gmail.com>
2017-09-21 19:08:44 +0200

    update dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: [["es2015", { modules: false }], "react"],
+            presets: [["env", { modules: false }], "react"],
             plugins: [require("babel-plugin-transform-object-rest-spread")]
           }
         }
