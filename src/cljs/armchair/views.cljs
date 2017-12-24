b0326b85e09af7c76ac64448582d4f78387d4205
Luciano Maiwald <luciano.maiwald@gmail.com>
2017-12-24 17:14:45 +0100

    move style into sass file :)

--- src/cljs/armchair/views.cljs
+++ src/cljs/armchair/views.cljs
@@ -1,29 +1,12 @@
 (ns armchair.views
   (:require [re-frame.core :as re-frame :refer [dispatch subscribe]]))
 
-(defn svg [width height & children]
-  [:svg {:version "1.1"
-         :baseProfile "full"
-         :style { :height "100%" :width "100%" }
-         :xmlns "http://www.w3.org/2000/svg"}
-   children])
-
 (defn line-component [{:keys [id text position]}]
   [:div {:on-mouse-down #(dispatch [:start-drag id])
          :on-mouse-up #(dispatch [:end-drag])
-         :style {:position "absolute"
-                 :-webkit-user-select "none"
-                 :box-sizing "border-box"
-                 :user-select "none"
-                 :width 200
-                 :min-height 30
-                 :border "1px solid black"
-                 :border-radius 15
-                 :background "white"
-                 :padding "5px 10px"
-                 :left (first position)
-                 :top (second position)
-                 }}
+         :className "line-component"
+         :style {:left (first position)
+                 :top (second position)}}
    text])
 
 (defn lines-component []
@@ -34,7 +17,9 @@
 
 (defn connections-component []
   (let [connections @(subscribe [:connections])]
-    [svg 800 800
+    [:svg {:version "1.1"
+           :baseProfile "full"
+           :xmlns "http://www.w3.org/2000/svg"}
      (for [{:keys [id start end]} connections]
        ^{:key id} [:line {:stroke "black"
                           :x1 (+ 200 (first start))
@@ -45,13 +30,6 @@
 
 (defn main-panel []
   [:div {:className "container"
-         :on-mouse-move #(dispatch [:move-pointer [(.. % -pageX) (.. % -pageY)]])
-         :style {:position "absolute"
-                 :top 0
-                 :left 0
-                 :height "100%"
-                 :width "100%"
-                 :overflow "hidden"
-                 }}
+         :on-mouse-move #(dispatch [:move-pointer [(.. % -pageX) (.. % -pageY)]])}
    [lines-component]
    [connections-component]])
