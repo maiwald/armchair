f46d1d2c10f4d1af93f5f2b9a808d5bd656bdbe3
Luciano Maiwald <luciano.maiwald@gmail.com>
2017-12-24 15:59:53 +0100

    update demo line positions

--- src/cljs/armchair/db.cljs
+++ src/cljs/armchair/db.cljs
@@ -1,21 +1,21 @@
 (ns armchair.db)
 
 (def default-db
-  {:lines '{
-            1  {:id 1  :character-id 1 :position [0   200] :text "Hey, who are you?"}
-            2  {:id 2  :character-id 2 :position [230 120] :text "I could ask you the same."}
-            3  {:id 3  :character-id 2 :position [230 200] :text "My name does not matter."}
-            4  {:id 4  :character-id 1 :position [460 120] :text "I am Hugo. And you...?"}
-            5  {:id 5  :character-id 2 :position [0   0  ] :text "I am Hugo as well."}
-            6  {:id 6  :character-id 2 :position [0   0  ] :text "None of your business!"}
-            7  {:id 7  :character-id 1 :position [0   0  ] :text "Fine, be a jerk."}
-            8  {:id 8  :character-id 1 :position [0   0  ] :text "Nice to meet you!"}
-            9  {:id 9  :character-id 1 :position [0   0  ] :text "Ok, bye!"}
-            10 {:id 10 :character-id 2 :position [230 280] :text "Hello Hugo!"}
-            11 {:id 11 :character-id 1 :position [0   0  ] :text "How do you know my name?"}
-            12 {:id 12 :character-id 2 :position [0   0  ] :text "We have met before. In the land far beyond."}
-            13 {:id 13 :character-id 1 :position [0   0  ] :text "Trying to sound ominous or what?! Get outa here!"}
-            }
+  {:lines {
+           1  {:id 1  :character-id 1 :position [   0 200] :text "Hey, who are you?"}
+           2  {:id 2  :character-id 2 :position [ 229 198] :text "I could ask you the same."}
+           3  {:id 3  :character-id 2 :position [ 259  91] :text "My name does not matter."}
+           4  {:id 4  :character-id 1 :position [ 450 214] :text "I am Hugo. And you...?"}
+           5  {:id 5  :character-id 2 :position [ 695 178] :text "I am Hugo as well."}
+           6  {:id 6  :character-id 2 :position [ 699 239] :text "None of your business!"}
+           7  {:id 7  :character-id 1 :position [ 939  90] :text "Fine, be a jerk."}
+           8  {:id 8  :character-id 1 :position [ 965 183] :text "Nice to meet you!"}
+           9  {:id 9  :character-id 1 :position [1216 197] :text "Ok, bye!"}
+           10 {:id 10 :character-id 2 :position [ 230 280] :text "Hello Hugo!"}
+           11 {:id 11 :character-id 1 :position [ 457 280] :text "How do you know my name?"}
+           12 {:id 12 :character-id 2 :position [ 764 314] :text "We have met before. In the land far beyond."}
+           13 {:id 13 :character-id 1 :position [1012 284] :text "Trying to sound ominous or what?! Get outa here!"}
+           }
    :connections #{
                   [1 2]
                   [2 4]
