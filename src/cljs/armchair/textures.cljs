522e3d1235333626d32a7bf0db56b3523111aba9
Luciano Maiwald <luciano.maiwald@gmail.com>
2018-11-10 23:36:47 +0100

    :heart: https://opengameart.org/content/dungeon-crawl-32x32-tiles

--- src/cljs/armchair/textures.cljs
+++ src/cljs/armchair/textures.cljs
@@ -52,17 +52,7 @@
                           :stone_grass_top-right
                           :stone])
 
-(def character-textures [:agent
-                         :dead_squirrel_idle
-                         :droog_idle
-                         :girl_idle
-                         :goth_idle
-                         :hugo
-                         :gustav
-                         :hi-tops_idle
-                         :punker
-                         :rourke
-                         :yakuza_idle])
+(def character-textures [:acid_blob :agent :agnes :aizul :angel :antaeus :asmodeus :azrael :azure_jelly :big_kobold :blork_the_orc :boggart :boris :brown_ooze :centaur :centaur-melee :centaur_warrior :centaur_warrior-melee :cerebov :crazy_yiuf :cyclops :daeva :dead_squirrel_idle :death_drake :deep_elf_annihilator :deep_elf_blademaster :deep_elf_conjurer :deep_elf_death_mage :deep_elf_demonologist :deep_elf_fighter :deep_elf_high_priest :deep_elf_knight :deep_elf_mage :deep_elf_master_archer :deep_elf_priest :deep_elf_soldier :deep_elf_sorcerer :deep_elf_summoner :deep_troll :deformed_elf :deformed_human :deformed_orc :demonspawn :dispater :dissolution :donald :dragon :droog_idle :duane :dwarf :edmund :elf :enchantress :ereshkigal :erica :erolcha :ettin :eustachio :eye_of_devastation :eye_of_draining :fire_drake :fire_giant :frances :francis :frederick :frost_giant :gastronok :geryon :giant_amoeba :giant_eyeball :giant_orange_brain :girl_idle :gloorx_vloq :glowing_shapeshifter :gnoll :goblin :golden_dragon :golden_eye :goth_idle :great_orb_of_eyes :greater_naga :griffon :grinder :grinder_cleaver :grum :guardian_serpent :gustav :halfling :harold :harpy :hell_knight :hi-tops_idle :hill_giant :hippogriff :hobgoblin :hugo :human :human_monk_ghost :human_slave :hydra1 :hydra2 :hydra3 :hydra4 :hydra5 :ice_beast :ice_dragon :ijyb :ilsuiw :ilsuiw_water :iron_dragon :iron_troll :iron_troll_monk_ghost :jelly :jessica :joseph :josephine :jozef :killer_klown :kirke :kobold :kobold_demonologist :lernaean_hydra :lindwurm :lom_lobon :louise :manticore :mara :margery :maud :maurice :menkaure :merfolk_aquamancer :merfolk_aquamancer_water :merfolk_fighter :merfolk_fighter_water :merfolk_impaler :merfolk_impaler_water :merfolk_javelineer :merfolk_javelineer_water :merfolk_plain :merfolk_plain_water :mermaid :mermaid_water :minotaur :mnoleg :mottled_dragon :murray :naga :naga_mage :naga_warrior :necromancer :nellie :nergalle :nessos :norbert :norris :ogre :ogre_mage :ooze :orb_guardian :orc :orc_high_priest :orc_knight :orc_priest :orc_sorcerer :orc_warlord :orc_warrior :orc_wizard :polyphemus :prince_ribbit :psyche :pulsating_lump :punker :purgy :quicksilver_dragon :rock_troll :rock_troll_monk_ghost :rourke :roxanne :royal_jelly :rupert :saint_roka :serpent_of_hell :shadow_dragon :shapeshifter :shining_eye :sigmund :siren :siren_water :snorg :sonja :sphinx :steam_dragon :stone_giant :storm_dragon :swamp_dragon :swamp_drake :terence :tiamat :titan :troll :two_headed_ogre :urug :vault_guard :wiglaf :wizard :wyvern :xtahua :yaktaur :yaktaur-melee :yaktaur_captain :yaktaur_captain-melee :yakuza_idle])
 
 (def texture-set (set (-> [:player
                            :enemy
