a686550320be6ceae4b83e3b45db26b1508f29eb
Luciano Maiwald <luciano.maiwald@gmail.com>
2019-03-21 19:23:30 +0100

    do not pass unnecessary data to game (including nils)

--- src/cljs/armchair/game/subs.cljs
+++ src/cljs/armchair/game/subs.cljs
@@ -1,5 +1,6 @@
 (ns armchair.game.subs
   (:require [clojure.spec.alpha :as s]
+            [clojure.set :refer [rename-keys]]
             [re-frame.core :refer [reg-sub]]
             [com.rpl.specter
              :refer [must ALL NONE MAP-VALS]
@@ -11,8 +12,10 @@
                                    :game/locations
                                    :game/initial-state]))
 (s/def :game/initial-state :armchair.game.core/state)
-(s/def :game/lines (s/map-of :entity/id (s/keys :req-un [:game/text :game/options])))
-(s/def :game/options (s/coll-of (s/keys :req-un [:game/text :game/next-line-id])
+(s/def :game/lines (s/map-of :entity/id (s/keys :req-un [:game/text :game/options]
+                                                :opt-un [:game/triggers])))
+(s/def :game/options (s/coll-of (s/keys :req-un [:game/text :game/next-line-id]
+                                        :opt-un [:game/triggers])
                                 :kind vector?))
 (s/def :game/locations (s/map-of :entity/id (s/keys :req-un [:game/dimension
                                                              :game/background
@@ -20,6 +23,12 @@
                                                              :game/outbound-connections
                                                              :game/inbound-connections
                                                              :game/npcs])))
+(s/def :game/triggers (s/keys :opt-un [:game-triggers/dialogue-states
+                                       :game-triggers/switches]))
+(s/def :game/trigger-map (s/map-of :entity/id :entity/id))
+(s/def :game-triggers/dialogue-states :game/trigger-map)
+(s/def :game-triggers/switches :game/trigger-map)
+
 (s/def :game/dimension :type/rect)
 (s/def :game/walk-set (s/coll-of :type/point :kind set?))
 (s/def :game/outbound-connections (s/map-of :type/point :game/location-id))
@@ -63,11 +72,13 @@
                        triggers (->> (:trigger-ids trigger)
                                      (map triggers)
                                      (group-by :switch-kind)
+                                     (u/filter-map some?)
                                      (u/map-values triggers-to-map))]
                    (assoc line
                           :next-line-id (:next-line-id trigger)
-                          :triggers {:dialogue-states (:dialogue-state triggers)
-                                     :switches (:switch triggers)})))
+                          :triggers (rename-keys triggers
+                                                 {:dialogue-state :dialogue-states
+                                                  :switch :switches}))))
                lines)))
 
 (reg-sub
@@ -83,11 +94,13 @@
                        triggers (->> (:trigger-ids trigger)
                                      (map triggers)
                                      (group-by :switch-kind)
+                                     (u/filter-map some?)
                                      (u/map-values triggers-to-map))]
                    (assoc player-option
                           :next-line-id (:next-line-id trigger)
-                          :triggers {:dialogue-states (:dialogue-state triggers)
-                                     :switches (:switch triggers)})))
+                          :triggers (rename-keys triggers
+                                                 {:dialogue-state :dialogue-states
+                                                  :switch :switches}))))
                player-options)))
 
 
@@ -97,16 +110,16 @@
   :<- [:game/player-options-with-triggers]
   (fn [[lines player-options]]
     (u/map-values
-      (fn [{:keys [triggers text next-line-id]}]
-        {:text text
-         :triggers triggers
-         :options (if-let [next-line (get lines next-line-id)]
-                    (case (:kind next-line)
-                      :npc (vector {:text "Continue..."
-                                    :next-line-id (:entity/id next-line)})
-                      :player (mapv player-options (:options next-line)))
-                    (vector {:text "Yeah..., whatever. Farewell"
-                             :next-line-id nil}))})
+      (fn [{:keys [next-line-id] :as line}]
+        (merge
+          (select-keys line [:text :triggers])
+          {:options (if-let [next-line (get lines next-line-id)]
+                      (case (:kind next-line)
+                        :npc (vector {:text "Continue..."
+                                      :next-line-id (:entity/id next-line)})
+                        :player (mapv player-options (:options next-line)))
+                      (vector {:text "Yeah..., whatever. Farewell"
+                               :next-line-id nil}))}))
       (u/where-map :kind :npc lines))))
 
 (reg-sub
