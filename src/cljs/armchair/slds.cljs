abcf219f5f936020d1522454c15d617040f17c42
Luciano Maiwald <luciano.maiwald@gmail.com>
2019-05-19 10:36:22 +0200

    add on-submit to modals

--- src/cljs/armchair/slds.cljs
+++ src/cljs/armchair/slds.cljs
@@ -1,5 +1,6 @@
 (ns armchair.slds
   (:require [reagent.core :as r]
+            [armchair.util :as u]
             [armchair.components :as c]))
 
 (defn form []
@@ -78,21 +79,21 @@
 (defn modal [{:keys [title close-handler confirm-handler width]}]
   [:div
    [:section {:class ["slds-modal"
-                      "slds-fade-in-open"
-                      (when (= width :medium) "slds-modal_medium")]}
+                      "slds-fade-in-open"]}
     [:div {:class "slds-modal__container"}
-     [:header {:class "slds-modal__header"}
-      [:button {:class "slds-button slds-button_icon slds-modal__close slds-button_icon-inverse"
-                :on-click close-handler
-                :title "Close"}
-       [:svg {:class "slds-button__icon slds-button__icon_large"}
-        [:use {:xlinkHref "assets/icons/utility-sprite/svg/symbols.svg#close", :xmlnsXlink "http://www.w3.org/1999/xlink"}]]]
-      [:h2 {:class "slds-text-heading_medium slds-hyphenate"} title]]
-     (into [:div {:class "slds-modal__content slds-p-around_medium"}]
-           (r/children (r/current-component)))
-     [:footer {:class "slds-modal__footer"}
-      [:button {:class "slds-button slds-button_brand"
-                :on-click (or confirm-handler
-                              close-handler)}
-       "Ok"]]]]
+     [:form {:on-submit (u/e-> confirm-handler)}
+       [:header {:class "slds-modal__header"}
+        [:button {:class "slds-button slds-button_icon slds-modal__close slds-button_icon-inverse"
+                  :on-click close-handler
+                  :type "button"
+                  :title "Close"}
+         [:svg {:class "slds-button__icon slds-button__icon_large"}
+          [:use {:xlinkHref "assets/icons/utility-sprite/svg/symbols.svg#close", :xmlnsXlink "http://www.w3.org/1999/xlink"}]]]
+        [:h2 {:class "slds-text-heading_medium slds-hyphenate"} title]]
+       (into [:div {:class "slds-modal__content slds-p-around_medium"}]
+             (r/children (r/current-component)))
+       [:footer {:class "slds-modal__footer"}
+        [:button {:class "slds-button slds-button_brand"
+                  :type "submit"}
+         "Ok"]]]]]
    [:div {:class "slds-backdrop slds-backdrop_open"}]])
