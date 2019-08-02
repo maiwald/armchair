4698d86a918864f0e2fafe0857c0c6769f420725
Luciano Maiwald <luciano.maiwald@gmail.com>
2019-08-02 22:56:01 +0200

    make synopsis editable

--- src/cljs/armchair/input.cljs
+++ src/cljs/armchair/input.cljs
@@ -38,15 +38,16 @@
       (for [[option option-label] options]
         [:option {:key (str id ":" option) :value option} option-label])]]))
 
-(defn textarea [{:keys [label on-change value]}]
+(defn textarea [{:keys [label on-change value options]}]
   (let [id (gensym "input-textarea")]
     [:div {:class "input input-textarea"}
      (when label
        [:label {:class "input-textarea__label input__label" :for id} label])
-     [:textarea {:class "input-textarea__textarea"
-                 :id id
-                 :on-change on-change
-                 :value value}]]))
+     [:textarea (merge options
+                       {:class "input-textarea__textarea"
+                        :id id
+                        :on-change on-change
+                        :value value})]]))
 
 (defn multiselect [{:keys [label on-change values options]}]
   (let [id (gensym "input-multiselect")]
