f8f62da5632708a26044acee257b8c2173c9e3d4
Luciano Maiwald <luciano.maiwald@gmail.com>
2019-12-26 12:47:24 +0100

    remove breadcrumb and resource dropdown
    in favour of a simpler navigation bar

--- src/cljs/armchair/views.cljs
+++ src/cljs/armchair/views.cljs
@@ -118,77 +118,46 @@
 ;; Navigation
 
 (defn navigation []
-  (let [dropdown-open? (r/atom false)
-        select (fn [resource]
-                 (>navigate resource)
-                 (swap! dropdown-open? not))]
-    (fn []
-      (let [{page-name :handler
-             page-params :route-params} (match-route routes (<sub [:current-page]))]
-        [:header {:id "global-header"}
-         [:div.logo "Armchair"]
-         [:nav
-          [:ul.main.navigation-container
-           [:li {:class ["navigation__item"
-                         (when (= page-name :game) "is-active")]}
-            [:a {:on-click #(>navigate :game)} "Play"]]
-           [:li {:class ["navigation__item"
-                         (when (= page-name :locations) "is-active")]}
-            [:a {:on-click #(>navigate :locations)}
-             (if (= page-name :game) "Edit" "Locations")]]]
-          (into [:ol.breadcrumb.navigation-container]
-                (let [{:keys [location dialogue]} (<sub [:breadcrumb])]
-                  [(if-let [{:keys [id display-name]} location]
-                     [:li.navigation__item
-                      {:class (when (= :location-edit page-name) "is-active")}
-                      [:a {:on-click #(>navigate :location-edit :id id)}
-                       [:span.navigation__item__type "Location"]
-                       [:span.navigation__item__title
-                        (u/truncate display-name 25)]]])
-                   (if-let [{:keys [id character-name synopsis]} dialogue]
-                     [:li.navigation__item
-                      {:class (when (= :dialogue-edit page-name) "is-active")}
-                      [:a {:on-click #(>navigate :dialogue-edit :id id)}
-                       [:span.navigation__item__type "Dialogue"]
-                       [:span.navigation__item__title
-                        (u/truncate (str character-name ": " synopsis) 25)]]])]))
-          (let [resource-pages {:dialogues "Dialogues"
-                                :characters "Characters"
-                                :switches "Switches"}
-                active? (contains? resource-pages page-name)]
-            [:div.resources {:class ["navigation__item"
-                                     (when active? "is-active")]}
-             [:a.resources__title {:on-click (fn [] (swap! dropdown-open? not))}
-              (when active?
-                [:span.navigation__item__type "Resources"])
-              [:span.navigation__item__title
-               (get resource-pages page-name "Resources")]
-              [icon "caret-down"]]
-             (when @dropdown-open?
-               [:ul.resources__dropdown-list
-                (for [[route resource] resource-pages]
-                  [:li {:key (str "resource-" route)}
-                   [:a {:on-click #(select route)} resource]])])])
-          [:ul.functions
-           [:li
-            (if (<sub [:can-undo?])
-              [:a {:on-click #(>evt [:undo])} [icon "undo"] "undo"]
-              [:span {:class "disabled"} [icon "undo"] "undo"])]
-           [:li
-            (if (<sub [:can-redo?])
-              [:a {:on-click #(>evt [:redo])} [icon "redo"] "redo"]
-              [:span {:class "disabled"} [icon "redo"] "redo"])]
-           [:li
-            [:a {:on-click #(>evt [:download-state])}
-             [icon "download"] "save to file"]]
-           [:li
-            [:a {:on-click #(u/upload-json! (fn [json] (>evt [:upload-state json])))}
-             [icon "upload"] "load from file"]]
-           [:li [:a {:on-click #(>evt [:reset-db])} "reset"]]
-           (when-not config/debug?
-             [:li [:a {:href "https://github.com/maiwald/armchair"
-                       :target "_blank"}
-                   [icon "code-branch"] "source"]])]]]))))
+  (let [{page-name :handler
+         page-params :route-params} (match-route routes (<sub [:current-page]))]
+    [:header {:id "global-header"}
+     [:div.logo "Armchair"]
+     [:nav
+      (if (= page-name :game)
+        [:ul.navigation
+         [:li {:class ["navigation__item"]}
+          [:a {:on-click #(>navigate :locations)} "Edit Game"]]]
+        [:ul.navigation
+         [:li {:class ["navigation__item"]}
+          [:a {:on-click #(>navigate :game)} "Play"]]
+         (for [[route title] [[:locations "Location Map"]
+                              [:dialogues "Dialogues"]
+                              [:characters "Characters"]
+                              [:switches "Switches"]]]
+           [:li {:key (str "navigation-item:" title)
+                 :class ["navigation__item"
+                         (when (= page-name route) "is-active")]}
+            [:a {:on-click #(>navigate route)} title]])])
+      [:ul.functions
+       [:li
+        (if (<sub [:can-undo?])
+          [:a {:on-click #(>evt [:undo])} [icon "undo"] "undo"]
+          [:span {:class "disabled"} [icon "undo"] "undo"])]
+       [:li
+        (if (<sub [:can-redo?])
+          [:a {:on-click #(>evt [:redo])} [icon "redo"] "redo"]
+          [:span {:class "disabled"} [icon "redo"] "redo"])]
+       [:li
+        [:a {:on-click #(>evt [:download-state])}
+         [icon "download"] "save to file"]]
+       [:li
+        [:a {:on-click (fn [e] (u/upload-json! #(>evt [:upload-state %])))}
+         [icon "upload"] "load from file"]]
+       [:li [:a {:on-click #(>evt [:reset-db])} "reset"]]
+       (when-not config/debug?
+         [:li [:a {:href "https://github.com/maiwald/armchair"
+                   :target "_blank"}
+               [icon "code-branch"] "source"]])]]]))
 
 ;; Root
 
