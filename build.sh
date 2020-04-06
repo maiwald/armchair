0d96a2c986b95b866e6b4680114f0ac77710b6ed
Luciano Maiwald <luciano.maiwald@gmail.com>
2020-04-06 08:33:16 +0200

    separate build and release scripts

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+source .env
+
+rm -fr $RELEASE_FOLDER/*
+
+mkdir -p $RELEASE_FOLDER/compiled/css
+mkdir -p $RELEASE_FOLDER/compiled/js
+
+ln -s ../resources/public/css/ $RELEASE_FOLDER/css
+ln -s ../resources/public/assets/ $RELEASE_FOLDER/assets
+ln -s ../resources/public/images/ $RELEASE_FOLDER/images
+ln -s ../resources/public/fonts/ $RELEASE_FOLDER/fonts
+ln -s ../resources/public/webfonts/ $RELEASE_FOLDER/webfonts
+
+lein cljsbuild once min
+sass --sourcemap=none src/sass/app.sass $RELEASE_FOLDER/compiled/css/app.css
+
+mv $RELEASE_FOLDER/compiled/css/app.css $RELEASE_FOLDER/compiled/css/app-$VERSION.css
+mv $RELEASE_FOLDER/compiled/js/app.js $RELEASE_FOLDER/compiled/js/app-$VERSION.js
+
+cp resources/public/index.html $RELEASE_FOLDER/index.html
+sed \
+  -e "s/app\.css/app-$VERSION.css/" \
+  -e "s/app\.js/app-$VERSION.js/" \
+  $RELEASE_FOLDER/index.html > $RELEASE_FOLDER/index.html.tmp &&
+  mv $RELEASE_FOLDER/index.html.tmp $RELEASE_FOLDER/index.html
--- release.sh
+++ release.sh
@@ -1,35 +1,9 @@
 #!/bin/bash
 
-VERSION=$(git rev-parse HEAD)
-RELEASE_FOLDER=build
-
 set -e
 
 source .env
-
-rm -fr $RELEASE_FOLDER/*
-
-mkdir -p $RELEASE_FOLDER/compiled/css
-mkdir -p $RELEASE_FOLDER/compiled/js
-
-ln -s ../resources/public/css/ $RELEASE_FOLDER/css
-ln -s ../resources/public/assets/ $RELEASE_FOLDER/assets
-ln -s ../resources/public/images/ $RELEASE_FOLDER/images
-ln -s ../resources/public/fonts/ $RELEASE_FOLDER/fonts
-ln -s ../resources/public/webfonts/ $RELEASE_FOLDER/webfonts
-
-lein cljsbuild once min
-sass --sourcemap=none src/sass/app.sass $RELEASE_FOLDER/compiled/css/app.css
-
-mv $RELEASE_FOLDER/compiled/css/app.css $RELEASE_FOLDER/compiled/css/app-$VERSION.css
-mv $RELEASE_FOLDER/compiled/js/app.js $RELEASE_FOLDER/compiled/js/app-$VERSION.js
-
-cp resources/public/index.html $RELEASE_FOLDER/index.html
-sed \
-  -e "s/app\.css/app-$VERSION.css/" \
-  -e "s/app\.js/app-$VERSION.js/" \
-  $RELEASE_FOLDER/index.html > $RELEASE_FOLDER/index.html.tmp &&
-  mv $RELEASE_FOLDER/index.html.tmp $RELEASE_FOLDER/index.html
+source build.sh
 
 aws s3 sync \
   --follow-symlinks \
