6c6d0aef5b882436c3bf6d78c9108ed297a98ed2
Luciano Maiwald <luciano.maiwald@gmail.com>
2020-06-18 17:42:13 +0200

    lift active ressource panel into app state
    the sidebar should stay open when switching to the game and back again

--- src/cljs/armchair/components/sidebar.cljs
+++ src/cljs/armchair/components/sidebar.cljs
@@ -1,21 +1,17 @@
 (ns armchair.components.sidebar
-  (:require [reagent.core :as r]
-            [armchair.components :as c]))
+  (:require [armchair.components :as c]))
 
-(defn sidebar [{active :active-panel}]
-  (let [active-panel (r/atom active)]
-    (fn [{:keys [panels]}]
-      [:aside.sidebar
-       [:ul.panel-selectors
-        (doall (for [[panel-key {:keys [icon label bottom?]}] panels
-                     :let [active? (= panel-key @active-panel)]]
-                [:li.panel-selector {:key (str "panel-selector-" panel-key)
-                                     :class [(when active? "is-active")
-                                             (when bottom? "is-bottom")]
-                                     :on-click (fn []
-                                                 (reset! active-panel
-                                                         (if active? nil panel-key)))}
-                 [c/icon icon label {:fixed? true}]]))]
-       (when (contains? panels @active-panel)
-         [:div.panel (get-in panels [@active-panel :component])])])))
-
+(defn sidebar [{:keys [panels active-panel on-panel-change on-panel-close]}]
+  [:aside.sidebar
+   [:ul.panel-selectors
+    (for [[panel-key {:keys [icon label bottom?]}] panels
+          :let [active? (= panel-key active-panel)]]
+      [:li.panel-selector {:key (str "sidebar-panel-selector-" panel-key)
+                           :class [(when active? "is-active")
+                                   (when bottom? "is-bottom")]
+                           :on-click (if (= active-panel panel-key)
+                                       on-panel-close
+                                       #(on-panel-change panel-key))}
+       [c/icon icon label {:fixed? true}]])]
+   (when (contains? panels active-panel)
+     [:div.panel (get-in panels [active-panel :component])])])
